9068bb0f35867e632af3b3f84c2cdfd8f1133199
dong@tjuacm <zhengdongjian@tju.edu.cn>
2015-11-20 16:53:04 +0800

    update pthread.c

--- 03threads/pthread.c
+++ 03threads/pthread.c
@@ -16,9 +16,11 @@
 
 #define NUMBER_OF_THREADS 10
 
+int x;
 void* print_hello_world(void* tid) {
 	//print the thread id (tid) and exit
 	printf("Hello world, greetings from thread %d, of tid %lld\n", *((int*)tid), (long long)pthread_self());
+	++x;
 	pthread_exit(NULL);
 	return NULL;
 }
@@ -28,17 +30,23 @@ int main(int argc, char* argv[]) {
 	pthread_t threads[NUMBER_OF_THREADS];
 	int status, i;
 	printf("main: %lld\n", (long long)pthread_self());
+	x = 0;
 	for (i = 0; i < NUMBER_OF_THREADS; ++i) {
 		printf("Main here, creating thread %d\n", i);
-		status = pthread_create(threads + i, NULL, &print_hello_world, (void*)(&i));
+		int* id = (int*)malloc(sizeof(int));
+		*id = i;
+		status = pthread_create(threads + i, NULL, &print_hello_world, (void*)(id));
+		sched_yield();
 
 		if (status != 0) {
 			printf("Oops, pthread_create returned error code %d\n", status);
 			break;
 		}
-		sched_yield();
-		//pthread_yield();
 		//pthread_join(threads[i], NULL);
 	}
-	return 0;
+	for (i = 0; i < NUMBER_OF_THREADS; ++i) {
+		pthread_join(threads[i], NULL);
+	}
+	printf("x = %d\n", x);
+	//return 0;
 }
